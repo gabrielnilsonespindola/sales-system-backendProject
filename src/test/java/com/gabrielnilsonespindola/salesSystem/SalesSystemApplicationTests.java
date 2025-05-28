fb7bae9c369a80e712edd2c720e0b933bae4f3de
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-28 18:05:21 -0300

    primeiro commit,projeto criado

---
+++ src/test/java/com/gabrielnilsonespindola/salesSystem/SalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielnilsonespindola.salesSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
