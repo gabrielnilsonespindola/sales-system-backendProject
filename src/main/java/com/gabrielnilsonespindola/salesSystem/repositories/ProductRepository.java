57fd9202044067bf502dbf3b434a4c6012c3cfdd
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-30 13:15:28 -0300

    ProductSale classes criadas

--- src/main/java/com/gabrielnilsonespindola/salesSystem/repositories/ProductRepository.java
+++ src/main/java/com/gabrielnilsonespindola/salesSystem/repositories/ProductRepository.java
@@ -2,8 +2,8 @@ package com.gabrielnilsonespindola.salesSystem.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.gabrielnilsonespindola.salesSystem.entities.Product;
+import com.gabrielnilsonespindola.salesSystem.entities.ProductSale;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<ProductSale, Long> {
 
 }
---
+++ src/main/java/com/gabrielnilsonespindola/salesSystem/services/ProductSaleService.java
@@ -0,0 +1,19 @@
+package com.gabrielnilsonespindola.salesSystem.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielnilsonespindola.salesSystem.repositories.ProductSaleRepository;
+
+@Service
+public class ProductSaleService {
+	
+	@Autowired
+	private ProductSaleRepository productSaleRepository;
+	
+	
+	
+	
+	
+
+}
