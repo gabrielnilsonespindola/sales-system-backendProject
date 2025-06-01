c48b0af6c245a3df9f074ac90b3ffadf9fba65bb
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-06-01 12:35:58 -0300

    role repository e seed atualizado com perfis role

---
+++ src/main/java/com/gabrielnilsonespindola/salesSystem/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.gabrielnilsonespindola.salesSystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.gabrielnilsonespindola.salesSystem.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
+INSERT INTO tb_role (role_id, role_Name) VALUES (1, 'admin');
+INSERT INTO tb_role (role_id, role_Name) VALUES (2, 'basic');
+
 INSERT INTO tb_product (name, price , stock_Quantity) VALUES ('TV', '200.0' , '10');
 INSERT INTO tb_product (name, price , stock_Quantity) VALUES ('Geladeira', '300.0' , '7');
 INSERT INTO tb_product (name, price , stock_Quantity) VALUES ('Fogão', '250.0' , '3');
