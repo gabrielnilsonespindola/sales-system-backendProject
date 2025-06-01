06b3fd580eaf80b061e441af3eb3aaa7104fcfb4
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-06-01 12:10:14 -0300

    role entity atualizaçao perfis de acesso enum

--- src/main/java/com/gabrielnilsonespindola/salesSystem/entities/Role.java
+++ src/main/java/com/gabrielnilsonespindola/salesSystem/entities/Role.java
@@ -3,6 +3,7 @@ package com.gabrielnilsonespindola.salesSystem.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,32 +11,33 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "/roles")
+@Table(name = "tb_role")
 public class Role implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long roleid;
+	@Column(name = "role_id")
+	private Long roleId;
 
 	private String roleName;
 	
 	public Role() {
 	}
 
-	public Role(Long roleid, String roleName) {
+	public Role(Long roleId, String roleName) {
 		super();
-		this.roleid = roleid;
+		this.roleId = roleId;
 		this.roleName = roleName;
 	}
 
 	public Long getRoleid() {
-		return roleid;
+		return roleId;
 	}
 
 	public void setRoleid(Long roleid) {
-		this.roleid = roleid;
+		this.roleId = roleid;
 	}
 
 	public String getRoleName() {
@@ -61,6 +63,29 @@ public class Role implements Serializable {
 			return false;
 		Role other = (Role) obj;
 		return Objects.equals(roleName, other.roleName);
+	}
+	
+	
+	 public enum Values {
+
+	        ADMIN(1L),
+	        BASIC(2L);
+
+	        long roleId;
+
+	        Values(){	        	
+	        }
+	        
+	        Values(long roleId) {
+	            this.roleId = roleId;
+	        }
+
+	        public long getRoleId() {
+	            return roleId;
+	        }
+		
+		
+		
 	}
 	
 	
