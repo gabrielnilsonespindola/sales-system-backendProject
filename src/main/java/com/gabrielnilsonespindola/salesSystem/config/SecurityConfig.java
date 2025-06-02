7a06f00a292112132f953a201c5bc2e9d67f3549
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-06-02 16:38:18 -0300

    chave privada e sua respectiva chave publica corrigidas, testado e gerado acess token, podendo assim enviar requisição de login OK

--- src/main/java/com/gabrielnilsonespindola/salesSystem/config/SecurityConfig.java
+++ src/main/java/com/gabrielnilsonespindola/salesSystem/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import java.security.interfaces.RSAPublicKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,7 +38,8 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
 		http
-        .authorizeHttpRequests(authorize -> authorize                
+        .authorizeHttpRequests(authorize -> authorize 
+        .requestMatchers(HttpMethod.POST,"/login").permitAll()
         .anyRequest().authenticated())
         .csrf(csrf -> csrf.disable())
         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
@@ -64,5 +66,9 @@ public class SecurityConfig {
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	
+	
+	
 
 }
