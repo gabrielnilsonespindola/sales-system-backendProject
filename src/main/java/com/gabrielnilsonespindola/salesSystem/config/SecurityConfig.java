76b47bd6148ec141d25301e2088fe4a059dcf1c8
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-06-03 13:33:31 -0300

    correção csrf disable para api rest via token rodar, banco h2 continua rodando e requisicoes http tambem,postman ok

--- src/main/java/com/gabrielnilsonespindola/salesSystem/config/SecurityConfig.java
+++ src/main/java/com/gabrielnilsonespindola/salesSystem/config/SecurityConfig.java
@@ -43,8 +43,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/login").permitAll()
                 .requestMatchers(HttpMethod.POST, "/users").permitAll()
                 .anyRequest().authenticated())
-                .csrf(csrf -> csrf
-                .ignoringRequestMatchers("/h2-console/**"))
+                .csrf(csrf -> csrf.disable())
                 .headers(headers -> headers
                 .frameOptions(frame -> frame.sameOrigin()))
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
