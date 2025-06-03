a011681b96b372c51ae53f6917e957ccddbc508d
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-06-03 13:16:51 -0300

    h2 banco test autorizado/rodando via spring security

--- src/main/java/com/gabrielnilsonespindola/salesSystem/config/SecurityConfig.java
+++ src/main/java/com/gabrielnilsonespindola/salesSystem/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.gabrielnilsonespindola.salesSystem.config;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +17,6 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -29,6 +27,7 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 @EnableMethodSecurity
 public class SecurityConfig {
 	
+		
 	@Value("${jwt.private.key}")
 	public RSAPrivateKey privateKey;
 	
@@ -36,21 +35,23 @@ public class SecurityConfig {
 	public RSAPublicKey publicKey;	
 	
 	
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		
-		http
-        .authorizeHttpRequests(authorize -> authorize 
-        .requestMatchers(HttpMethod.POST,"/login").permitAll()
-        .requestMatchers(HttpMethod.POST,"/users").permitAll()
-        .anyRequest().authenticated())
-        .csrf(csrf -> csrf.disable())
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-		
-		return http.build();
-		
-	}
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                .anyRequest().authenticated())
+                .csrf(csrf -> csrf
+                .ignoringRequestMatchers("/h2-console/**"))
+                .headers(headers -> headers
+                .frameOptions(frame -> frame.sameOrigin()))
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 	
 	@Bean
 	public JwtEncoder jwtEncoder() {
@@ -69,9 +70,6 @@ public class SecurityConfig {
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	
-	
 	
 
 }
